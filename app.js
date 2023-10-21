29cda47c7939dabd744c81e18768f529316002fc
ch1llmaster <118458860+ch1llmaster@users.noreply.github.com>
2023-10-21 23:27:08 +0300

    js change

--- app.js
+++ app.js
@@ -17,78 +17,66 @@ let btn6 = document.getElementById("btn6");
 btn1.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 1!") {
         tg.MainButton.hide();
-        btn1.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 1!");
         item = "1";
         tg.MainButton.show();
-        btn1.data = "Выбрано";
     }
 });
 
 btn2.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 2!") {
         tg.MainButton.hide();
-        btn2.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 2!");
         item = "2";
         tg.MainButton.show();
-        btn2.data = "Выбрано";
     }
 });
 
 btn3.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 3!") {
         tg.MainButton.hide();
-        btn3.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 3!");
         item = "3";
         tg.MainButton.show();
-        btn3.data = "Выбрано";
     }
 });
 
 btn4.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 4!") {
         tg.MainButton.hide();
-        btn4.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 4!");
         item = "4";
         tg.MainButton.show();
-        btn4.data = "Выбрано";
     }
 });
 
 btn5.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 5!") {
         tg.MainButton.hide();
-        btn5.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 5!");
         item = "5";
         tg.MainButton.show();
-        btn5.data = "Выбрано";
     }
 });
 
 btn6.addEventListener("click", function(){
     if (tg.MainButton.isVisible && tg.MainButton.text === "Вы выбрали товар 6!") {
         tg.MainButton.hide();
-        btn6.data = "Примерить";
     }
     else {
         tg.MainButton.setText("Вы выбрали товар 6!");
         item = "6";
         tg.MainButton.show();
-        btn6.data = "Выбрано";
     }
 });
 
